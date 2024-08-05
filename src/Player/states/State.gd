351cbba92ca924089922e3ce7dc805c34c7cac5c
Riley HP <rhend@proton.me>
2024-08-05 13:09:44 -0300

    migrated to LFS

---
+++ src/Player/states/State.gd
@@ -0,0 +1,17 @@
+class_name State extends Node
+
+var _player : Player
+func _init_state(player : Player):
+	_player = player
+
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+func process_state(delta):
+	pass
+	
+func interrupt():
+	pass
